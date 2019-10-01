382e9114d32aafdc58c54236377a7a774fe766e3
Brad Farberman <bfarberman@gmail.com>
2019-10-01 19:45:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,8 @@ def map(source_array)
   source_array.each do |number|
     source_array[index] = yield(number)
     index += -1 
-  
-  it "returns an array with all values made negative" do
-    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
+  source_array
+end 
+  
 
