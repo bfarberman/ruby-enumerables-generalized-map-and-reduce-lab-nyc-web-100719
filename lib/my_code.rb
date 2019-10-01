c7c7427036b8bf567e7dda406b11af6ce2bd8cb1
Brad Farberman <bfarberman@gmail.com>
2019-10-01 19:49:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,7 +7,7 @@ def map(source_array)
   source_array
 end 
 
-def reduce(array)
+def reduce(array, starting_point)
  
   it "returns an array with the original values" do
     dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
